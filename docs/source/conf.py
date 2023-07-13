52f0ac462c4e6d545a27a3bbf273e1242292ccae
Ben Blair <benchrisblair@users.noreply.github.com>
2023-07-13 13:11:37 -0700

    Add version to sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,16 @@
 project = "Django Filtered Model Formset Tables"
 copyright = "2023, powderflask"
 author = "Joseph Fall"
+
+# The short X.Y version.
+version = "0.1.0"
+# The full version, including alpha/beta/rc tags.
+release = "0.1.0"
+
+# The master toctree document.
 master_doc = "index"
+
+# The suffix(es) of source filenames.
 source_suffix = [".rst", ".md"]
 
 # -- General configuration ---------------------------------------------------
@@ -24,7 +33,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".venv", ".tox"]
+exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
