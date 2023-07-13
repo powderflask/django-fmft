4ed1893f17eaa437ce562b185094c9fe11122e08
Ben Blair <benchrisblair@users.noreply.github.com>
2023-07-13 13:12:15 -0700

    Add invoke tasks to manage dependencies

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,32 +1,5 @@
-try:
-    import tomllib
-except ModuleNotFoundError:
-    import tomli as tomllib
-
-from invoke import Collection, task
-
-from . import clean, docs, pypi
+from invoke import task, Collection
+from . import clean, deps, docs, pypi
 
 
-@task
-def install_deps(c):
-    # Load pyproject.toml file
-    with open("pyproject.toml", "r") as f:
-        pyproject = tomllib.load(f)
-
-    # Install core dependencies
-    if "dependencies" in pyproject["project"]:
-        for dep in pyproject["project"]["dependencies"]:
-            c.run(f"python -m pip install {dep}")
-
-    # Install optional dependencies
-    if "optional-dependencies" in pyproject["project"]:
-        for opt_dep_list in pyproject["project"]["optional-dependencies"].values():
-            for opt_dep in opt_dep_list:
-                c.run(f"python -m pip install {opt_dep}")
-
-    print("Dependencies installed successfully.")
-
-
-namespace = Collection(clean, docs, pypi)
-namespace.add_task(install_deps)
+namespace = Collection(clean, deps, docs, pypi)
---
+++ tasks/deps.py
@@ -0,0 +1,35 @@
+from invoke import task
+
+
+@task
+def pin(c, optional=False, docs=False):
+    """ Pin all core [and optional] dependencies from pyproject.toml """
+    print("Generating requirements files...")
+    c.run("pip-compile --resolver=backtracking -o requirements.txt pyproject.toml")
+    if optional:
+        c.run("pip-compile --resolver=backtracking --all-extras -o requirements_dev.txt pyproject.toml")
+    if docs:
+        c.run("pip-compile --resolver=backtracking --extra=docs -o docs/requirements_docs.txt pyproject.toml")
+    print("Done.")
+
+
+@task
+def upgrade(c, optional=False, docs=False):
+    """ Force update all core [and optional] dependencies in requirements files """
+    print("Updating requirements files...")
+    c.run("pip-compile --resolver=backtracking --upgrade -o requirements.txt pyproject.toml")
+    if optional:
+        c.run("pip-compile --resolver=backtracking --all-extras --upgrade -o requirements_dev.txt pyproject.toml")
+    if docs:
+        c.run("pip-compile --resolver=backtracking --extra=docs --upgrade -o docs/requirements_docs.txt pyproject.toml")
+    print("Done.")
+
+
+@task
+def install(c, optional=False):
+    """ Install all core [and optional] dependencies """
+    print("Installing dependencies...")
+    c.run("pip-sync requirements.txt")
+    if optional:
+        c.run("pip-sync requirements_dev.txt")
+    print("Done.")
