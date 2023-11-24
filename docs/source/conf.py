7baadf5482c2f21dcba0a35cb594bd837a4625d5
powderflask <powderflask@gmail.com>
2023-11-24 10:45:57 -0800

    Bump version: 0.3.1 --> 0.3.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,9 +16,9 @@ copyright = "2023, powderflask"
 author = "Joseph Fall"
 
 # The short X.Y version.
-version = "0.3.1"
+version = "0.3.2"
 # The full version, including alpha/beta/rc tags.
-release = "0.3.1"
+release = "0.3.2"
 
 # The master toctree document.
 master_doc = "index"
--- fmft/__init__.py
+++ fmft/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Joseph Fall"""
 __email__ = "powderflask@gmail.com"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 from .views import (
     FilteredModelFormsetTableView,
